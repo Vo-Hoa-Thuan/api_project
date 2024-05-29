8ab042f3c81d96e4f24decba97cff63d1ca75209
Ykkshi2110 <quocbui2110@gmail.com>
2024-05-29 14:34:52 +0700

    update nguoidung api by Quoc 2:34Pm 29/5

--- controllers/nguoidung.controller.js
+++ controllers/nguoidung.controller.js
@@ -7,12 +7,12 @@ module.exports = {
     });
   },
 
-  getById: (req, res) => {
-    const ma_nguoi_dung = req.params.ma_nguoi_dung;
-    NguoiDung.getById(ma_nguoi_dung, (result) => {
-      res.send(result);
-    });
-  },
+  // getById: (req, res) => {
+  //   const ma_nguoi_dung = req.params.ma_nguoi_dung;
+  //   NguoiDung.getById(ma_nguoi_dung, (result) => {
+  //     res.send(result);
+  //   });
+  // },
 
   insert: (req, res) => {
     const nguoiDung = req.body;
@@ -36,55 +36,64 @@ module.exports = {
     });
   },
 
-  getAllByTenPhong: (req, res) => {
-    const tenPhong = req.query.tenPhong;
-    NguoiDung.getAllByTenPhong(tenPhong, (result) => {
-      res.send(result);
-    });
-  },
+  // getAllByTenPhong: (req, res) => {
+  //   try {
+  //     const tenPhong = req.params.tenphong;
+  //     NguoiDung.getAllByTenPhong(tenPhong, (err, result) => {
+  //       if (err) {
+  //         console.error("Lỗi khi truy vấn dữ liệu:", err);
+  //         return res.status(500).json({ error: "Đã xảy ra lỗi khi truy vấn dữ liệu" });
+  //       }
+  //       res.json(result);
+  //     });
+  //   } catch (error) {
+  //     console.error("Lỗi khi xử lý yêu cầu:", error);
+  //     res.status(500).json({ error: "Đã xảy ra lỗi khi xử lý yêu cầu" });
+  //   }
+  // },
 
   getAllByMaKhu: (req, res) => {
-    const maKhu = req.query.maKhu;
+    const maKhu = req.params.maKhu;
     NguoiDung.getAllByMaKhu(maKhu, (result) => {
       res.send(result);
     });
   },
 
   getNguoiDungByMaPhong: (req, res) => {
-    const maPhong = req.query.maPhong;
+    const maPhong = decodeURI(req.query.maPhong);
     NguoiDung.getNguoiDungByMaPhong(maPhong, (result) => {
       res.send(result);
     });
   },
 
-  getTenNguoiDungByMaPhong1: (req, res) => {
-    const maPhong = req.params.maPhong;
-    NguoiDung.getTenNguoiDungByMaPhong(maPhong, (result) => {
-      res.send(result);
-    });
-  },
+  // getTenNguoiDungByMaPhong1: (req, res) => {
+  //   const maPhong = req.params.maPhong;
+  //   NguoiDung.getTenNguoiDungByMaPhong(maPhong, (result) => {
+  //     res.send(result);
+  //   });
+  // },
 
-  getNguoiDungByTrangThai: (req, res) => {
-    const trangThai = req.query.trangThai;
-    const maPhong = req.query.maPhong;
-    NguoiDung.getNguoiDungByTrangThai(trangThai, maPhong, (result) => {
-      res.send(result);
-    });
-  },
+  // getNguoiDungByTrangThai: (req, res) => {
+  //   const trangThai = req.query.trangThai;
+  //   const maPhong = req.query.maPhong;
+  //   NguoiDung.getNguoiDungByTrangThai(trangThai, maPhong, (result) => {
+  //     res.send(result);
+  //   });
+  // },
 
-  getTenNguoiDungByMaPhong: (req, res) => {
-    const maPhong = req.params.maPhong;
-    NguoiDung.getTenNguoiDungByMaPhong(maPhong, (result) => {
-      res.send(result);
-    });
-  },
+  // getTenNguoiDungByMaPhong: (req, res) => {
+  //   const maPhong = req.params.maPhong;
+  //   NguoiDung.getTenNguoiDungByMaPhong(maPhong, (result) => {
+  //     res.send(result);
+  //   });
+  // },
 
-  getSoNguoiDungByMaPhong: (req, res) => {
-    const maPhong = req.params.maPhong;
-    NguoiDung.getSoNguoiDungByMaPhong(maPhong, (result) => {
-      res.send({ soNguoiDung: result });
-    });
-  },
+  // getSoNguoiDungByMaPhong: (req, res) => {
+  //   const maPhong = req.params.maPhong;
+  //   NguoiDung.getSoNguoiDungByMaPhong(maPhong, (result) => {
+  //     res.send({ soNguoiDung: result });
+  //   });
+  // },
 
   getSoNguoiOByMaPhong: (req, res) => {
     const maPhong = req.params.maPhong;
@@ -100,16 +109,26 @@ module.exports = {
     });
   },
 
-  getListTrangThaiHDDungByMaPhong: (req, res) => {
-    const maPhong = req.params.maPhong;
-    NguoiDung.getListTrangThaiHDDungByMaPhong(maPhong, (result) => {
-      res.send(result);
-    });
-  },
+  // getListTrangThaiHDDungByMaPhong: (req, res) => {
+  //   const maPhong = req.params.maPhong;
+  //   NguoiDung.getListTrangThaiHDDungByMaPhong(maPhong, (result) => {
+  //     res.send(result);
+  //   });
+  // },
+
+  // getAllInNguoiDangOByMaKhu: (req, res) => {
+  //   const maKhu = req.params.maKhu;
+  //   NguoiDung.getAllInNguoiDangOByMaKhu(maKhu, (result) => {
+  //     res.send(result);
+  //   });
+  // },
 
   getAllInNguoiDangOByMaKhu: (req, res) => {
     const maKhu = req.params.maKhu;
-    NguoiDung.getAllInNguoiDangOByMaKhu(maKhu, (result) => {
+    NguoiDung.getAllInNguoiDangOByMaKhu(maKhu, (err, result) => {
+      if (err) {
+        return res.status(500).send(err);
+      }
       res.send(result);
     });
   },
@@ -128,12 +147,12 @@ module.exports = {
     });
   },
 
-  getTenNguoiDaOByMaPhong: (req, res) => {
-    const maPhong = req.params.maPhong;
-    NguoiDung.getTenNguoiDaOByMaPhong(maPhong, (result) => {
-      res.send(result);
-    });
-  },
+  // getTenNguoiDaOByMaPhong: (req, res) => {
+  //   const maPhong = req.params.maPhong;
+  //   NguoiDung.getTenNguoiDaOByMaPhong(maPhong, (result) => {
+  //     res.send(result);
+  //   });
+  // },
 
   getMaNguoiDangOByMaPhong: (req, res) => {
     const maPhong = req.params.maPhong;
